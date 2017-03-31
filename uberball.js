de8c6eadc18f4b0830bd1043843c6b311b2d520b
apmansur <apmansur1993@gmail.com>
2017-03-30 21:32:21 -0500

    working solution refactored

--- uberball.js
+++ uberball.js
@@ -1,45 +1,20 @@
-function findBall(scales, ball_count) {
+function findBall(scale, ball_count) {
   var arrBalls=[];
-  var remainder=arrBalls.length%3;
-  var third = Math.round(arrBalls.length/3);
+  
    
   for (var i=0; i<ball_count; i++){
     arrBalls.push(i);
   }
-  while (arrBalls.length>2){
-    if (remainder === 0){
-      var A= arrBalls.slice(0,third);
-      var B= arrBalls.slice(third,(third*2));
-      var C= arrBalls.slice((third*2), arrBalls.length);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-      var scale= getWeight(A,B);
-        switch(scale){
-          case 1:
-            arrBalls=B;
-            break;
-          case 0:
-            arrBalls=C;
-            break;
-          case -1:
-            arrBalls=A;
-          break;
-          default:
-           return "something went wrong";
-        }
-    }
-    if (arrBalls.length==2 ){
-      var A =arrBalls[0];
-      var B= arrBalls[1];
-      var scale= getWeight(A,B);
-      switch(scale){
-          case 1:
-            arrBalls=B;
-            break;
-          case -1:
-            arrBalls=A;
-          break;
-          default:
-           return "something went wrong 2";   
-    }
   
+  while (arrBalls.length>1){
+    var third = Math.ceil(arrBalls.length/3);
+    var a = arrBalls.slice(0,third);
+    var b = arrBalls.slice(third, third * 2);
+    var c=  arrBalls.slice(a.length + b.length, arrBalls.length);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    var result = scale.getWeight(a,b);
+    if (result == -1) arrBalls = a;
+    else if(result == 1) arrBalls = b;
+    else arrBalls = c;
+  }
   return arrBalls[0];
 }
